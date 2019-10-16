8982774d3e3ed67be03701ff24baaa8957695633
Kaeks <jakey.jjm@gmail.com>
2019-10-16 14:54:29 +0200

    Add Fields

--- app/main.js
+++ app/main.js
@@ -63,6 +63,15 @@
 		}
 	}
 
+	class FieldPosition extends Position {
+		constructor(x, y) {
+			if (x !== Math.floor(x) || y !== Math.floor(y) || x < 0 || x > 2 || y < 0 || x > 2) {
+				throw 'wrong';
+			}
+			super(x, y);
+		}
+	}
+
 	class GameObject {
 		position;
 
@@ -78,6 +87,129 @@
 
 		}
 
+	}
+
+	class Field extends GameObject {
+		cellSize;
+		cells;
+		cellGap;
+		drawBorder;
+
+		constructor(position, cellSize, cellGap, drawBorder) {
+			super(position);
+			this.cellSize = cellSize;
+			this.cells = [];
+			this.cellGap = cellGap ? cellGap : 0;
+			this.drawBorder = drawBorder === true;
+			for (let i = 0; i < 3; i++) {
+				for (let j = 0; j < 3; j++) {
+					this.cells.push(new FieldCell(this, new FieldPosition(i, j), cellSize));
+				}
+			}
+		}
+
+		getPositionFromFieldPosition(fieldPosition) {
+			return new Position(
+				this.position.x + (this.cellGap + this.cellSize) * (fieldPosition.x - 1),
+				this.position.y + (this.cellGap + this.cellSize) * (fieldPosition.y - 1),
+			);
+		}
+
+		getArea() {
+			let totalGap = this.drawBorder ? this.cellGap * 2 : this.cellGap;
+			return new Area(
+				new Position(
+					this.position.x - this.cellSize * 1.5 - totalGap,
+					this.position.y - this.cellSize * 1.5 - totalGap
+				),
+				new Position(
+					this.position.x + this.cellSize * 1.5 + totalGap,
+					this.position.y + this.cellSize * 1.5 + totalGap
+				)
+			);
+		}
+
+		draw() {
+			super.draw();
+			context.save();
+			context.fillStyle = '#222';
+			this.getArea().fill();
+			context.restore();
+		}
+	}
+
+	class FieldCell extends GameObject {
+		field;
+		fieldPosition;
+		size;
+
+		constructor(field, fieldPosition, size) {
+			super(field.getPositionFromFieldPosition(fieldPosition));
+			this.field = field;
+			this.fieldPosition = fieldPosition;
+			this.size = size;
+		}
+
+		getArea() {
+			return new Area(
+				new Position(
+					this.position.x - this.size * 0.5,
+					this.position.y - this.size * 0.5
+				),
+				new Position(
+					this.position.x + this.size * 0.5,
+					this.position.y + this.size * 0.5
+				)
+			);
+		}
+
+		draw() {
+			super.draw();
+			super.draw();
+			context.save();
+			context.fillStyle = '#555';
+			this.getArea().fill();
+			context.restore();
+		}
+
+		highlight() {
+			let area = this.getArea();
+			context.fillStyle = '#777';
+			area.fill();
+		}
+	}
+
+	class FieldObject extends GameObject {
+		field;
+		fieldPosition;
+
+		constructor(field, fieldPosition) {
+			super(field.getPositionFromFieldPosition(fieldPosition));
+			this.field = field;
+			this.fieldPosition = fieldPosition;
+		}
+	}
+
+	class Symbol extends FieldObject {
+		size;
+
+		constructor(field, fieldPosition, size) {
+			super(field, fieldPosition);
+			this.size = size;
+		}
+
+	}
+
+	class SymbolX extends Symbol {
+		constructor(field, fieldPosition, size) {
+			super(field, fieldPosition, size);
+		}
+	}
+
+	class SymbolO extends Symbol {
+		constructor(field, fieldPosition, size) {
+			super(field, fieldPosition, size);
+		}
 	}
 
 	// END CLASS DECLARATIONS
@@ -102,6 +234,7 @@
 	 * @param {Array} [args] A List of arguments
 	 */
 	function runForAllIn(func, inside, reverse, args) {
+		if (inside === undefined) return;
 		if (reverse === true) {
 			for (let i = inside.length - 1; i >= 0; i--) {
 				func(inside[i], args);
@@ -159,6 +292,30 @@
 		runForSpecificIn(func, type, gameObjectList, reverse, args);
 	}
 
+	/**
+	 * Get a list of the FieldCells that are currently being pressed
+	 *
+	 * @returns {[]}
+	 */
+	function determineCellsPressed() {
+		if (mousePosition === undefined) return undefined;
+		if (mouseEvents[0] !== true) return undefined;
+		let list = [];
+		for (let i = 0; i < gameObjectList.length; i++) {
+			let cur = gameObjectList[i];
+			if (!(cur instanceof Field)) continue;
+			if (mousePosition.isWithin(cur.getArea())) {
+				for (let j = 0; j < cur.cells.length; j++) {
+					let cell = cur.cells[j];
+					if (mousePosition.isWithin(cell.getArea())) {
+						list.push(cell);
+					}
+				}
+			}
+		}
+		return list;
+	}
+
 	// INPUT LISTENERS
 
 	let mouseEvents = {};
@@ -189,7 +346,19 @@
 
 		runForAll(function(cur) {
 			cur.draw();
+			if (cur instanceof Field) {
+				runForAllIn(function(cur) {
+					cur.draw();
+					}, cur.cells
+				);
+			}
 		});
+
+		runForAllIn(
+			function(cur) {
+				cur.highlight();
+			}, determineCellsPressed(), false
+		);
 
 		requestAnimationFrame(gameLoop);
 	}
@@ -204,6 +373,11 @@
 		canvas.width = canvas.clientWidth;
 		canvas.height = canvas.clientHeight;
 		context.imageSmoothingEnabled = false;
+
+		// OBJECT SETUP
+
+		mainField = new Field(getCanvasCenterPosition(), 100, 10, true);
+		gameObjectList.push(mainField);
 
 		addEventListener('mousedown', mouseListener);
 		addEventListener('mouseup', mouseListener);
