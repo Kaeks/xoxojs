cbc51e181a0a8ff23a78cfe335d07048cde4a75d
Kaeks <jakey.jjm@gmail.com>
2019-10-17 15:34:37 +0200

    Add ability to place symbols, winner determination

--- app/main.js
+++ app/main.js
@@ -54,11 +54,17 @@
 			return this.getWidth() * this.getHeight();
 		}
 
-		fill() {
+		contains(position) {
+			return position.isWithin(this);
+		}
+
+		fill(style) {
+			context.fillStyle = style;
 			context.fillRect(this.x1, this.y1, this.getWidth(), this.getHeight());
 		}
 
-		stroke() {
+		stroke(style) {
+			context.strokeStyle = style;
 			context.strokeRect(this.x1, this.y1, this.getWidth(), this.getHeight());
 		}
 	}
@@ -237,22 +243,60 @@
 
 	}
 
+	class CellList {
+		cells;
+
+		constructor(cells) {
+			this.cells = cells;
+		}
+
+		hasWinner() {
+			let first;
+			for (let i = 0; i < this.cells.length; i++) {
+				if (this.cells[i].symbol === undefined) return false;
+				let cur = this.cells[i].symbol.type;
+				//if (cur === undefined) return false;
+				if (i === 0) first = cur;
+				if (cur !== first) return false;
+			}
+			// We've established that this row is a winning row
+			for (let i = 0; i < this.cells.length; i++) {
+				this.cells[i].hasWin = true;
+			}
+			return true;
+		}
+
+		getWinner() {
+			if (this.hasWinner()) return this.cells[0].type;
+		}
+
+	}
+
+	class Row extends CellList {
+		constructor(cells) {
+			super(cells);
+		}
+	}
+
 	class Field extends GameObject {
 		cellSize;
-		cells;
+		rows;
 		cellGap;
 		drawBorder;
 
 		constructor(position, cellSize, cellGap, drawBorder) {
 			super(position);
 			this.cellSize = cellSize;
-			this.cells = [];
+			this.rows = [];
 			this.cellGap = cellGap ? cellGap : 0;
 			this.drawBorder = drawBorder === true;
+
 			for (let i = 0; i < 3; i++) {
+				let cells = [];
 				for (let j = 0; j < 3; j++) {
-					this.cells.push(new FieldCell(this, new FieldPosition(i, j), cellSize));
+					cells.push(new FieldCell(this, new FieldPosition(j, i), cellSize));
 				}
+				this.rows.push(new Row(cells));
 			}
 		}
 
@@ -277,53 +321,67 @@
 			);
 		}
 
+		getColumn(col) {
+			let list = [];
+
+			for (let i = 0; i < this.rows.length; i++) {
+				list.push(this.rows[i].cells[col]);
+			}
+
+			return new CellList(list);
+		}
+
+		getDiagonal(whichOneHuh) {
+			let list = [];
+			for (let i = 0; i < this.rows.length; i++) {
+				if (whichOneHuh === 0) {
+					list.push(this.rows[i].cells[i]);
+				} else if (whichOneHuh === 1) {
+					list.push(this.rows[i].cells[this.rows.length - 1 - i]);
+				}
+			}
+			return new CellList(list);
+		}
+
+		analyzeWins() {
+			let wins = [];
+
+			for (let i = 0; i < this.rows.length; i++) {
+				let curRow = this.rows[i];
+				let curCol = this.getColumn(i);
+				if (i <= 1) {
+					let curDiag = this.getDiagonal(i);
+					if (curDiag.hasWinner()) wins.push(curDiag);
+				}
+				if (curRow.hasWinner()) wins.push(curRow);
+				if (curCol.hasWinner()) wins.push(curCol);
+			}
+
+			return wins;
+		}
+
+		determineWinner(wins) {
+			let list = [];
+			for (let i = 0; i < wins.length; i++) {
+				list.push(wins[i].getWinner());
+			}
+			return list;
+		}
+
+		update() {
+			super.update();
+			for (let i = 0; i < this.rows.length; i++) {
+				let row = this.rows[i];
+				for (let j = 0; j < row.cells.length; j++) {
+					let cell = row.cells[j];
+					cell.isHighlighted = mousePosition.isWithin(cell.getArea()) && mouseEvents[0] === true;
+				}
+			}
+		}
+
 		draw() {
 			super.draw();
-			context.save();
-			context.fillStyle = '#222';
-			this.getArea().fill();
-			context.restore();
-		}
-	}
-
-	class FieldCell extends GameObject {
-		field;
-		fieldPosition;
-		size;
-
-		constructor(field, fieldPosition, size) {
-			super(field.getPositionFromFieldPosition(fieldPosition));
-			this.field = field;
-			this.fieldPosition = fieldPosition;
-			this.size = size;
-		}
-
-		getArea() {
-			return new Area(
-				new Position(
-					this.position.x - this.size * 0.5,
-					this.position.y - this.size * 0.5
-				),
-				new Position(
-					this.position.x + this.size * 0.5,
-					this.position.y + this.size * 0.5
-				)
-			);
-		}
-
-		draw() {
-			super.draw();
-			super.draw();
-			context.save();
-			context.fillStyle = '#555';
-			this.getArea().fill();
-			context.restore();
-		}
-
-		highlight() {
-			let area = this.getArea();
-			context.fillStyle = '#777';
-			area.fill();
+			this.getArea().fill('#222');
 		}
 	}
 
@@ -338,25 +396,114 @@
 		}
 	}
 
-	class Symbol extends FieldObject {
+	class FieldCell extends FieldObject {
 		size;
+		symbol;
+		isHighlighted;
+		hasWin;
 
 		constructor(field, fieldPosition, size) {
 			super(field, fieldPosition);
 			this.size = size;
+			this.isHighlighted = false;
+			this.hasWin = false;
+		}
+
+		getArea() {
+			return new Area(new Position(this.position.x - this.size * 0.5, this.position.y - this.size * 0.5), new Position(this.position.x + this.size * 0.5, this.position.y + this.size * 0.5));
+		}
+
+		setSymbol(symbol) {
+			this.symbol = symbol;
+		}
+
+		determineColor() {
+			if (this.isHighlighted) {
+				if (this.hasWin) {
+					if (this.symbol.type === 'X') {
+						return '#855';
+					} else if (this.symbol.type === 'O') {
+						return '#558';
+					}
+				}
+				return '#777';
+			} else {
+				if (this.hasWin) {
+					if (this.symbol.type === 'X') {
+						return '#744';
+					} else if (this.symbol.type === 'O') {
+						return '#447';
+					}
+				}
+			}
+			return '#555';
+		}
+
+		draw() {
+			super.draw();
+			let style = this.determineColor();
+			this.getArea().fill(style);
+			if (this.symbol !== undefined) this.symbol.draw();
+		}
+	}
+
+	class CellObject extends GameObject {
+		constructor(cell, cellPosition) {
+			super(new Position(cell.position.x + cellPosition.x, cell.position.y + cellPosition.y));
+		}
+	}
+
+	class Symbol extends CellObject {
+		size;
+		type;
+
+		constructor(cell, cellPosition, size, type) {
+			super(cell, cellPosition);
+			this.size = size;
+			this.type = type;
+		}
+
+		draw() {
+			super.draw();
+			// can't draw this abstract object
 		}
 
 	}
 
 	class SymbolX extends Symbol {
-		constructor(field, fieldPosition, size) {
-			super(field, fieldPosition, size);
+		shape;
+
+		constructor(cell, cellPosition, size) {
+			super(cell, cellPosition, size, 'X');
+			this.shape = new Polygon([
+				[ -3, -3 ], [ -2, -3 ], [ 0, -1 ], [ 2, -3 ],
+				[ 3, -3 ], [ 3, -2 ], [ 1, 0 ], [ 3, 2 ],
+				[ 3, 3 ], [ 2, 3 ], [ 0, 1 ], [ -2, 3 ],
+				[ -3, 3 ], [ -3, 2 ], [ -1, 0 ], [ -3, -2 ]
+			]);
+			let width = this.shape.getWidth();
+			this.shape = this.shape.scale(size / width);
+		}
+
+		draw() {
+			super.draw();
+			context.fillStyle = '#b44';
+			this.shape.drawAt(this.position, true, false);
 		}
 	}
 
 	class SymbolO extends Symbol {
-		constructor(field, fieldPosition, size) {
-			super(field, fieldPosition, size);
+		constructor(cell, cellPosition, size) {
+			super(cell, cellPosition, size, 'O');
+			this.shape = new Circle(1);
+			let width = this.shape.getWidth();
+			this.shape = this.shape.scale(size / width);
+		}
+
+		draw() {
+			super.draw();
+			context.strokeStyle = '#44b';
+			this.shape.drawAt(this.position, false, true, 15);
 		}
 	}
 
@@ -445,18 +592,21 @@
 	 *
 	 * @returns {[]}
 	 */
-	function determineCellsPressed() {
-		if (mousePosition === undefined) return undefined;
-		if (mouseEvents[0] !== true) return undefined;
+
+	function getCellsAt(position) {
+		if (position === undefined) return undefined;
 		let list = [];
 		for (let i = 0; i < gameObjectList.length; i++) {
 			let cur = gameObjectList[i];
 			if (!(cur instanceof Field)) continue;
-			if (mousePosition.isWithin(cur.getArea())) {
-				for (let j = 0; j < cur.cells.length; j++) {
-					let cell = cur.cells[j];
-					if (mousePosition.isWithin(cell.getArea())) {
-						list.push(cell);
+			if (position.isWithin(cur.getArea())) {
+				for (let j = 0; j < cur.rows.length; j++) {
+					let row = cur.rows[j];
+					for (let k = 0; k < row.cells.length; k++) {
+						let cell = row.cells[k];
+						if (position.isWithin(cell.getArea())) {
+							list.push(cell);
+						}
 					}
 				}
 			}
@@ -464,10 +614,32 @@
 		return list;
 	}
 
+	function attemptPlaceSymbol(position, type) {
+		let list = getCellsAt(position);
+		if (list.length === 0) return false;
+		let cell = list[0];
+		if (cell.symbol !== undefined) return false;
+		let symbol;
+		switch (type) {
+			case 'X':
+				symbol = new SymbolX(cell, new Position(0, 0), 90);
+				break;
+			case 'O':
+				symbol = new SymbolO(cell, new Position(0, 0), 80);
+				break;
+			default:
+				return false;
+		}
+		cell.setSymbol(symbol);
+		cell.field.analyzeWins();
+		return true;
+	}
+
 	// INPUT LISTENERS
 
 	let mouseEvents = {};
-	let mousePosition = new Position(0,0);
+	let mousePosition = new Position(0, 0);
+	let currentPlaying;
 
 	function mouseListener(e) {
 		switch (e.type) {
@@ -476,6 +648,18 @@
 				break;
 			case 'mouseup':
 				mouseEvents[e.button] = false;
+				if (attemptPlaceSymbol(mousePosition, currentPlaying)) {
+					let previousPlaying = currentPlaying;
+					currentPlaying = undefined;
+					switch (previousPlaying) {
+						case 'X':
+							currentPlaying = 'O';
+							break;
+						case 'O':
+							currentPlaying = 'X';
+							break;
+					}
+				}
 				break;
 			case 'mousemove':
 				mousePosition = new Position(e.clientX, e.clientY);
@@ -488,25 +672,24 @@
 	let mainField;
 
 	function gameLoop() {
+		// necessary for chrome devtools
+		gameObjectList = gameObjectList;
+		// UPDATE ALL
 		runForAll(function(cur) {
 			cur.update();
 		});
 
+		// DRAW ALL
 		runForAll(function(cur) {
 			cur.draw();
 			if (cur instanceof Field) {
 				runForAllIn(function(cur) {
-					cur.draw();
-					}, cur.cells
-				);
+					runForAllIn(function(cur) {
+						cur.draw();
+					}, cur.cells);
+				}, cur.rows);
 			}
 		});
-
-		runForAllIn(
-			function(cur) {
-				cur.highlight();
-			}, determineCellsPressed(), false
-		);
 
 		requestAnimationFrame(gameLoop);
 	}
@@ -523,15 +706,21 @@
 		context.imageSmoothingEnabled = false;
 
 		// OBJECT SETUP
-
 		mainField = new Field(getCanvasCenterPosition(), 100, 10, true);
 		gameObjectList.push(mainField);
 
+		// LOGIC SETUP
+		currentPlaying = 'X';
+
+		// LISTENER SETUP
 		addEventListener('mousedown', mouseListener);
 		addEventListener('mouseup', mouseListener);
 		addEventListener('mousemove', mouseListener);
-		addEventListener('contextmenu', function(e){e.preventDefault();}, false);
+		addEventListener('contextmenu', function(e) {
+			e.preventDefault();
+		}, false);
 
+		// GAME START
 		gameLoop();
 	}
 
